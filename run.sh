0149f477da51b4b4a8fde30c4e519eb49aaa19e3
Copykop404 <81780677+Copykop404@users.noreply.github.com>
2021-04-01 22:20:05 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,24 @@
 phone='phone.txt'
-#Message Action
-for i in 0 1 2 3 4
-do
-   while read p
-     do
-       python __m.py $p $i
-     done < $phone
-done
-echo "Sleeping for 60 seconds"
-sleep 60
-
 #Visit Action
-for i in 0 1 2 3 4
+for i in 0
 do
    while read p
      do
        python __v.py $p $i
      done < $phone
 done
-echo "Sleeping for 60 seconds"
-sleep 60
+echo "Sleeping for 10 seconds"
+sleep 10
 
 #Join Action
-for i in 0 1 2 3 4
+for i in 0
 do
    while read p
      do
        python __j.py $p $i
      done < $phone
 done
-echo "Sleeping for 60 seconds"
-sleep 60
+echo "Sleeping for 10 seconds"
+sleep 10
 
 bash run.sh
