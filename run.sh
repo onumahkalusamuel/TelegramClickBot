543a80b2c79a33808d71329b630365125d4e9da6
onumahkalusamuel <onumahkalusamuel@gmail.com>
2021-10-16 08:02:23 +0100

    litecoin only

--- run.sh
+++ run.sh
@@ -1,44 +1,38 @@
 phone='phone.txt'
 
 #Join Action
-for i in 0 1 2 3 4
-do
-   while read p
-     do
-       if command -v python3 &> /dev/null
-       then python3 __j.py $p $i
-       else python __j.py $p $i
-       fi
-     done < $phone
-done
+while read p
+  do
+    if command -v python3 &> /dev/null
+    then python3 __j.py $p 1
+    else python __j.py $p 1
+    fi
+  done < $phone
+
 echo "Sleeping for 30 seconds"
 sleep 30
 
 #Message Action
-for i in 0 1 2 3 4
-do
-   while read p
-     do
-       if command -v python3 &> /dev/null
-       then python3 __m.py $p $i
-       else python __m.py $p $i
-       fi
-     done < $phone
-done
+while read p
+  do
+    if command -v python3 &> /dev/null
+    then python3 __m.py $p 1
+    else python __m.py $p 1
+    fi
+  done < $phone
+
 echo "Sleeping for 30 seconds"
 sleep 30
 
 #Visit Action
-for i in 0 1 2 3 4
-do
-   while read p
-     do
-       if command -v python3 &> /dev/null
-       then python3 __v.py $p $i
-       else python __v.py $p $i
-       fi
-     done < $phone
-done
+while read p
+  do
+    if command -v python3 &> /dev/null
+    then python3 __v.py $p 1
+    else python __v.py $p 1
+    fi
+  done < $phone
+
 echo "Sleeping for 30 seconds"
 sleep 30
 
